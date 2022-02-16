1d0d1e17f85a68171bcffa4dec083ea5460b91c0
Erik Luebbers <erluebbers@gmail.com>
2022-02-16 07:39:08 -0800

    commit tree 1ba788cff141202d89b98f2dd1c38e707d57f266

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,8 @@ require 'pry'
 
 class Artist
   extend Memorable
+  extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :songs
@@ -11,10 +13,6 @@ class Artist
   def initialize
     @@artists << self
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
@@ -30,7 +28,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
